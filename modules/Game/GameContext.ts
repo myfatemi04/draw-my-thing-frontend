677818033531f620c3cc4de0a4c2df54139f6c0d
Michael Fatemi <myfatemi04@gmail.com>
2021-05-28 01:23:34 -0400

    add GameProvider

--- modules/Game/GameContext.ts
+++ modules/Game/GameContext.ts
@@ -1,8 +1,10 @@
 import { createContext } from "react";
 import GameSDK from "./GameSDK";
+import GameState from "./GameState";
 
 const GameContext = createContext({
   gameSDK: new GameSDK(),
+  gameState: new GameState(),
 });
 
 export default GameContext;
---
+++ modules/Game/GameProvider.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useEffect, useMemo, useState } from "react";
+import GameContext from "./GameContext";
+import GameSDK from "./GameSDK";
+import GameState from "./GameState";
+
+function GameProvider({ children }: { children: ReactNode }) {
+  const [state, setState] = useState<GameState>();
+  const sdk = useMemo(() => new GameSDK(), []);
+
+  useEffect(() => {
+    sdk.on("state-update", (state) => {
+      setState(state);
+    });
+  }, []);
+
+  return (
+    <GameContext.Provider value={{ gameSDK: sdk, gameState: state }}>
+      {children}
+    </GameContext.Provider>
+  );
+}
+
+export default GameProvider;
