15576906b14c5be1b57c2c09b3394ac2ed034719
Michael Fatemi <myfatemi04@gmail.com>
2021-05-28 01:13:49 -0400

    add player record

---
+++ modules/Game/Player.ts
@@ -0,0 +1,26 @@
+import * as immutable from "immutable";
+
+class Player extends immutable.Record({
+  id: "",
+  nickname: "",
+  points: 0,
+}) {
+  constructor(id: string, nickname: string, points = 0) {
+    if (!id) {
+      throw new Error("invalid id: " + id);
+    }
+    if (!nickname) {
+      throw new Error("invalid nickname: " + nickname);
+    }
+    if (points < 0) {
+      throw new Error("invalid point count: " + points);
+    }
+    super({
+      id,
+      nickname,
+      points,
+    });
+  }
+}
+
+export default Player;
